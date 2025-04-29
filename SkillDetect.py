081c8abae0138a47506ff195d40127ef735c9193
StevenHatcher <stevenhatchersk@gmail.com>
2025-04-29 17:54:39 -0600

    Update SkillDetect.py

--- SkillDetect.py
+++ SkillDetect.py
@@ -6,7 +6,7 @@ import pyautogui #Used to find the crosshair icon on the screen.
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd # Import pandas to store the data from the website into a spreadsheet
-import  time # Import schedule and time to run every 30 seconds and check if text is on the screen
+import time # Import schedule and time to run every 30 seconds and check if text is on the screen
 from FortniteStats.FortniteStats import * # Import all of the functions from the FortniteStats repo
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" # (Optional) Set the Tesseract path if it's not in the system PATH
 
@@ -32,7 +32,6 @@ def check_if_eliminated():
             return False
     except pyautogui.ImageNotFoundException: # if the crosshair image isn't found, return without doing anything.
         return False
-
 
 # This function will calculate where the enemy's username will display on the screen based on where the crosshair is present on the screen  
 def calculate_username_region(crosshair_location):
@@ -97,51 +96,6 @@ def player_eliminated():
             return None
     except pyautogui.ImageNotFoundException: # if the crosshair image isn't found, return without doing anything.
         return
-    
-#--------------------------------- SPREADSHEET FUNCTIONS ---------------------------------#
-
-#function to initialize the spreadsheet if you don't have a blank one. This expects there to be a clean fortnite_stats.xlsx in generated to work.
-def init_spreadsheet(filename="fortnite_stats"):
-      # Create a new DataFrame with headers and a reserved row for averages
-            df = pd.DataFrame(columns=["K/D", "Playtime"])
-            df.loc[0] = [None, None]  # Row 0 for averages
-            df.to_excel(f"generated/{filename}.xlsx", index=False)
-
-# function to update the average of the stats of players that have eliminated in the spreadsheet
-def update_averages(filename="fortnite_stats"):
-    df = pd.read_excel(f"generated/{filename}.xlsx") # Load the spreadsheet
-
-    if len(df) <= 2: # If there arent two or more values in the spreadsheet, you cant really take an average lol
-        print("Not enough data to calculate averages yet.")
-        return
-
-    # Calculate averages only from data rows AFTER the reserved average row (starting from index 2)
-    data_rows = df.iloc[2:]
-    avg_kd = data_rows["K/D"].mean()
-    avg_playtime = data_rows["Playtime"].mean()
-    avg_playtime =  round(avg_playtime, 2)
-    avg_kd = round(avg_kd, 2)
-    # Update the reserved second row (index 0 when we don't consider the title as an indexed row) 
-    df.loc[0, "K/D"] = avg_kd
-    df.loc[0, "Playtime"] = avg_playtime
-
-    # Save back to Excel
-    df.to_excel(f"generated/{filename}.xlsx", index=False)
-
-# Write the enemy's kd and playtime to the spreadsheet.
-def write_to_spreadsheet(e_playtime, e_kd):#, p_playtime, p_kd):
-    if enemy_hours == 0 and enemy_kd == 0: return
-    # Load the existing spreadsheet
-    df = pd.read_excel("generated/fortnite_stats.xlsx")
-
-    # Append the new stats
-    new_row = {"K/D": e_kd, "Playtime": e_playtime}
-    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-
-    # Save back to Excel
-    df.to_excel("generated/fortnite_stats.xlsx", index=False)
-    print(f"Added: K/D = {e_kd}, Playtime = {e_playtime} hours")
-    update_averages() # Update the average K/D and Playtime of enemies in the first row.
 
 
 # main_screen, left_of_monitor, top_of_monitor = calculate_screen_region() # Get the region of the user's main screen
@@ -154,15 +108,12 @@ while True:
             enemy_name: str = player_eliminated() # Get the username of the enemy that eliminated you
             # I understand that this can create a problem if the same player eliminates you twice in a row... If that happens, I'll just uninstall the game and not worry about stats anymore lol.
             if enemy_name != "" and enemy_name != previous_text:
-                enemy_data = get_player_data(username=enemy_name, maximize=True) # Get JSON of the enemy player's stats
+                enemy_data = get_player_data(username=enemy_name, maximize=True, auto_close=False) # Get JSON of the enemy player's stats
                 if enemy_data: # If enemy data was successfully acquired, calculate their kd and playtime then write them to the spreadhseet
                     enemy_kd = get_player_stats(enemy_data, gamemode="all", data="KD", option="value") 
                     enemy_hours = round(get_player_stats(enemy_data, gamemode="all", data="MinutesPlayed", option="value") / 60, 2) # Get the player's playtime and convert it to hours
 
-                    write_to_spreadsheet(enemy_hours, enemy_kd) # Write the data to the spreadsheet, this will update the average at the top of the sheet as a side effect
-                
-                previous_text = enemy_name
-                update_averages() # Update the average K/D and Playtime of enemies in the first row.
+                previous_text = enemy_name # Set the variable that keeps track of the previous enemy's name to the name of the enemy that eliminated you.
             is_player_eliminated = False
             time.sleep(120) # Sleep for 2 minutes after getting killed and running program. Feel free to adjust this.
         else:
